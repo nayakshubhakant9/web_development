88f3474375d6df29195b6fb3d3d977904db60493
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-07-20 19:23:10 +0530

    init

---
+++ VIDEO 3/script.js
@@ -0,0 +1,1 @@
+alert("Welcome to shubhakant nayak website")
---
+++ VIDEO 58/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+// const func1 = (x)=>{
+//     console.log("I am an arrow function", x)
+// }
+
+// func1(34);
+// func1(66);
+// func1(84);
